b63f824e5ce7fc4cada20d7536a3fee93ab75bcb
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-08-24 10:03:01 -0700

    Update strava-js.js

--- strava-js.js
+++ strava-js.js
@@ -1,41 +1,21 @@
-import StravaApiV3 from "./strava-api.js";
+// Strava API endpoint and access token 
+const API_URL = 'https://www.strava.com/api/v3/athlete/activities';
+const ACCESS_TOKEN = '0bd0d49161189cc7bf570a01eeb1833a8f6df06d';
 
-const stravaClient = new StravaApiV3({
-  clientId: "111503",
-  clientSecret: "9abdda73b9cf15100b12faefcae56d4c8343a378",
-  accessToken: "0bd0d49161189cc7bf570a01eeb1833a8f6df06d",
-});
+// Fetch latest activity
+fetch(API_URL + '?per_page=1&access_token=' + ACCESS_TOKEN)
+  .then(response => response.json())
+  .then(data => {
 
-const getMostRecentActivity = async () => {
-  const activities = await stravaClient.getActivities({
-    per_page: 1,
-  });
+    // Get the first/latest activity
+    const activity = data[0];
 
-  return activities[0];
-};
+    // Display activity data    
+    document.getElementById('activity').innerHTML = `
+      <h2>${activity.name}</h2>
+      <p><strong>Distance:</strong> ${activity.distance} m</p>
+      <p><strong>Moving Time:</strong> ${activity.moving_time} s</p>
+    `;
 
-const renderActivity = (activity) => {
-  const html = `
-    <h1>My Most Recent Strava Activity</h1>
-    <p>
-      <strong>Name:</strong> ${activity.name}
-    </p>
-    <p>
-      <strong>Distance:</strong> ${activity.distance} meters
-    </p>
-    <p>
-      <strong>Time:</strong> ${activity.elapsed_time} seconds
-    </p>
-  `;
-
-  return html;
-};
-
-const main = async () => {
-  const activity = await getMostRecentActivity();
-  const html = renderActivity(activity);
-
-  document.querySelector("#activity").innerHTML = html;
-};
-
-main();
+  })
+  .catch(error => console.error(error));
