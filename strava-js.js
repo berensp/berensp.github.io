a29669f94111a3e55091a961c3c9c97715b3a5c9
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-08-25 11:04:16 -0700

    Update strava-js.js

--- strava-js.js
+++ strava-js.js
@@ -1,46 +1,65 @@
+// Strava API endpoints
+const AUTHORIZE_URL = 'https://www.strava.com/oauth/authorize';
+const TOKEN_URL = 'https://www.strava.com/oauth/token';
+const ACTIVITIES_URL = 'https://www.strava.com/api/v3/athlete/activities'; 
 
-const auth_link = "https://www.strava.com/oauth/token"
+// Client ID and secret from Strava API registration
+const CLIENT_ID = '111503';
+const CLIENT_SECRET = '9abdda73b9cf15100b12faefcae56d4c8343a378';
 
-function getActivites(res){
+// Saved refresh token 
+let refreshToken = 'your_refresh_token'; 
 
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
-    fetch(activities_link)
-        .then(response => response.json())
-        .then(data => {
+// Request scopes from Strava API
+const SCOPES = 'activity:read';
 
-        // Get the first/latest activity
-        const activity = data[0];
+async function fetchActivities() {
 
-        // Display activity data    
-        document.getElementById('activity').innerHTML = `
-          <h2>${activity.name}</h2>
-          <p><strong>Distance:</strong> ${activity.distance} m</p>
-          <p><strong>Moving Time:</strong> ${activity.moving_time} s</p>
-    `;
+  // Get access token
+  const accessToken = await getAccessToken();
+  
+  // Use access token to call Strava API
+  const activities = await fetch(ACTIVITIES_URL + '?access_token=' + accessToken)
+    .then(res => res.json());
 
-  })
-  .catch(error => console.error(error));
+  // Display first activity
+  displayActivity(activities[0]);
+
 }
 
-function reAuthorize(){
-    fetch(auth_link,{
-        method: 'post',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
+async function getAccessToken() {
 
-        },
+  // Request access token using refresh token
+  const response = await fetch(TOKEN_URL, {
+    method: 'POST',
+    headers: {
+       'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+      scope: SCOPES
+    })
+  });
+  
+  // Extract access token 
+  const data = await response.json();
+  return data.access_token;
 
-        body: JSON.stringify({
-
-            client_id: '111503',
-            client_secret: '9abdda73b9cf15100b12faefcae56d4c8343a378',
-            refresh_token: '5d7d5c4edf9d990542ad399d1c9a61d409611923',
-            grant_type: 'refresh_token'
-
-        })
-    }).then(res => res.json())
-        .then(res => getActivites(res))  
 }
 
-reAuthorize()
+function displayActivity(activity) {
+
+  // Display activity data
+  document.getElementById('activity').innerHTML = `
+    <h2>${activity.name}</h2>  
+    <p><strong>Distance:</strong> ${activity.distance} m</p>
+    <p><strong>Moving Time:</strong> ${activity.moving_time} s</p>
+  `;
+
+}
+
+// Initialize
+fetchActivities();
