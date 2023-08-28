656be7cdf5d51ef4804804422a7cb247a7247783
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-08-28 14:48:26 -0700

    strava update
    chatGPT help

--- strava-js.js
+++ strava-js.js
@@ -3,72 +3,77 @@ const AUTHORIZE_URL = 'https://www.strava.com/oauth/authorize';
 const TOKEN_URL = 'https://www.strava.com/oauth/token';
 const ACTIVITIES_URL = 'https://www.strava.com/api/v3/athlete/activities';
 
-// Application credentials 
-const CLIENT_ID = '111503';
-const CLIENT_SECRET = '9abdda73b9cf15100b12faefcae56d4c8343a378';
+// Strava API endpoints
+const STRAVA_API = {
+  AUTHORIZE_URL: 'https://www.strava.com/oauth/authorize',
+  TOKEN_URL: 'https://www.strava.com/oauth/token',
+  ACTIVITIES_URL: 'https://www.strava.com/api/v3/athlete/activities'
+};
+
+// Application credentials
+const CLIENT_CREDENTIALS = {
+  CLIENT_ID: '111503',
+  CLIENT_SECRET: '9abdda73b9cf15100b12faefcae56d4c8343a378'
+};
 
 // Request scopes
-const SCOPES = 'activity:read_all'; 
+const SCOPES = 'activity:read_all';
 
-// Redirect URI 
-const REDIRECT_URI = 'https://berens.co/strava-test';
+// Redirect URI
+const REDIRECT_URI = '/callback.html';
 
 // Refresh token stored in localStorage
 let refreshToken = localStorage.getItem('stravaRefreshToken');
 
+// Function to open authorization window
+function openAuthWindow(authUrl) {
+  return new Promise((resolve) => {
+    const popup = window.open(authUrl, 'authWindow', 'width=500,height=600');
+    const interval = setInterval(() => {
+      if (popup.closed) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 500);
+  });
+}
+
 // Get auth code
 async function getAuthCode() {
-
-  const authUrl = `${AUTHORIZE_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPES}`;
-
-  window.location = authUrl; // Redirect to auth endpoint
-
+  const authUrl = `${STRAVA_API.AUTHORIZE_URL}?client_id=${CLIENT_CREDENTIALS.CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPES}`;
+  await openAuthWindow(authUrl);
   const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get('code'); 
-
+  return urlParams.get('code');
 }
 
 // Get access token
 async function getAccessToken(code) {
-
-  const response = await fetch(TOKEN_URL, {
+  const response = await fetch(STRAVA_API.TOKEN_URL, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
+      ...CLIENT_CREDENTIALS,
       grant_type: 'authorization_code',
-      code: code,
-      redirect_uri: REDIRECT_URI,
-      refresh_token: refreshToken, 
-      scope: SCOPES
+      code,
+      redirect_uri: REDIRECT_URI
     })
   });
-
-  // Get access token and refresh token
-  const {access_token, refresh_token} = await response.json();
-
-  // Store new refresh token
-  localStorage.setItem('stravaRefreshToken', refresh_token);  
-
+  const { access_token, refresh_token } = await response.json();
+  localStorage.setItem('stravaRefreshToken', refresh_token);
   return access_token;
-
 }
-
 
 // Fetch activities
 async function fetchActivities() {
-
-  const code = await getAuthCode();
-  const accessToken = await getAccessToken(code);
-
-  const activities = await fetch(`${ACTIVITIES_URL}?access_token=${accessToken}`);
-  const data = await activities.json();
-
-  displayActivity(data[0]); // Display first activity
-
+  try {
+    const code = await getAuthCode();
+    const accessToken = await getAccessToken(code);
+    const activitiesResponse = await fetch(`${STRAVA_API.ACTIVITIES_URL}?access_token=${accessToken}`);
+    const activitiesData = await activitiesResponse.json();
+    displayActivity(activitiesData[0]); // Display first activity
+  } catch (error) {
+    console.error('Error fetching activities:', error);
+  }
 }
 
 // Initialize app
