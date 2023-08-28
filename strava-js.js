461760f6d71becf55e8bd405e9eaf04a1e4ac63e
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-08-28 15:40:39 -0700

    Update strava-js.js

--- strava-js.js
+++ strava-js.js
@@ -1,9 +1,4 @@
 // Strava API endpoints
-const AUTHORIZE_URL = 'https://www.strava.com/oauth/authorize';
-const TOKEN_URL = 'https://www.strava.com/oauth/token';
-const ACTIVITIES_URL = 'https://www.strava.com/api/v3/athlete/activities';
-
-// Strava API endpoints
 const STRAVA_API = {
   AUTHORIZE_URL: 'https://www.strava.com/oauth/authorize',
   TOKEN_URL: 'https://www.strava.com/oauth/token',
