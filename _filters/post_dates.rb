b03bfc7f78d8e28ebe833f07fec44524237c05c9
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-09-18 10:24:32 -0700

    Update post_dates.rb

--- _filters/post_dates.rb
+++ _filters/post_dates.rb
@@ -1,27 +1,35 @@
-Liquid::Template.register_filter(:post_last_modified_at) do |input|
-  # Get the path to the blog post file.
-  post_file_path = input.path
+module PostDates
+  def post_file_path(input)
+    posts_folder = '_posts'
+    filename = File.basename(input.path)
+    File.join(posts_folder, filename)
+  end 
+end
 
-  # Get the last modified date of the blog post file.
+Liquid::Template.register_filter(:post_last_modified_at) do |input|
+  # Construct file path
+  post_file_path = PostDates.post_file_path(input)
+
+  # Get last modified date
   post_last_modified_date = File.mtime(post_file_path)
 
-  # Convert the date to a Liquid date object.
+  # Convert to Liquid date
   liquid_date = Liquid::Date.parse(post_last_modified_date)
 
-  # Return the formatted date.
+  # Return formatted date
   liquid_date.strftime('%Y-%m-%d')
 end
 
 Liquid::Template.register_filter(:post_originally_posted_at) do |input|
-  # Get the path to the blog post file.
-  post_file_path = input.path
-
-  # Get the first commit date of the blog post file.
+  # Construct file path
+  post_file_path = PostDates.post_file_path(input)
+  
+  # Get original post date
   post_originally_posted_date = `git log #{post_file_path} -1 --format=%cd`.chomp
 
-  # Convert the date to a Liquid date object.
+  # Convert to Liquid date
   liquid_date = Liquid::Date.parse(post_originally_posted_date)
 
-  # Return the formatted date.
-  liquid_date.strftime('%Y-%m-%d')
+  # Return formatted date 
+  liquid_date.strftime('%Y-%m-%d') 
 end
