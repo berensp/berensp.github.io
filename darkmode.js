ed9c51c08482130f8d1d078d9e31a2844761616d
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-10-23 00:11:28 -0700

    Delete darkmode.js

--- darkmode.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.querySelector('.dark-mode-toggle');
-
-function toggleDarkMode() {
-  const body = document.querySelector('body');
-  body.classList.toggle('dark-mode');
-}
-
-toggleButton.addEventListener('click', toggleDarkMode);
