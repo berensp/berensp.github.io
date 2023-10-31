1bc3d5456ae3fa666824abe1f0ad0e064f177aca
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-10-31 13:15:38 -0700

    Update timediff.js

--- timediff.js
+++ timediff.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const postDateEl = document.querySelector('[data-post-date]');
-  
-  // Set date on load
-  const currentDate = new Date();
-  postDateEl.setAttribute('data-post-date', currentDate.toISOString());
 
-  // Get date  
-  const postDate = new Date(postDateEl.getAttribute('data-post-date'));
+  // Set date on load
+  const currentDate = new Date(); 
+  postDateEl.setAttribute('data-post-date', currentDate.toISOString());
   
-  // Calculate and display difference
-  const diffMs = Date.now() - postDate;
-  const diffDays = diffMs / (1000 * 60 * 60 * 24);
-  document.getElementById('timeDifference').textContent = `Time difference: ${diffDays} days`;
+  // Log date
+  console.log('Set postDate:', currentDate); 
 
 });
+
+// Get date
+const postDateEl = document.querySelector('[data-post-date]');
+const postDate = new Date(postDateEl.getAttribute('data-post-date'));
+
+// Log date 
+console.log('Post date:', postDate);
+
+// Calculate difference
+const diffMs = Date.now() - postDate;
+const diffDays = diffMs / (1000 * 60 * 60 * 24); 
+
+// Log difference
+console.log('Time difference in days:', diffDays);
+
+// Display difference
+const diffEl = document.querySelector('#timeDifference');
+diffEl.textContent = `Time difference: ${diffDays} days`;
