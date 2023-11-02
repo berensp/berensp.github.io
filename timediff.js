087949d8d67687c8a77caae146cb4a3afecb273f
Paul Berens <41165838+berensp@users.noreply.github.com>
2023-11-02 10:19:01 -0700

    timediff
    new crack at it

--- timediff.js
+++ timediff.js
@@ -1,40 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const postDateEl = document.querySelector('[data-post-date]');
+  const postDateEl = document.querySelector('#postDate');
   
   if (!postDateEl) {
     console.error('Post date element not found');
     return;
   }
 
-  const currentDate = new Date();
-
-  // Validate ISO format  
-  const isoRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/; 
-  const isoDate = currentDate.toISOString();
-
-  if (!isoDate.match(isoRegex)) {
-    console.error('Invalid ISO date:', isoDate);
-    throw new Error('Invalid ISO date format');
-  }
-
-  postDateEl.setAttribute('data-post-date', isoDate);
-
   const dateStr = postDateEl.getAttribute('data-post-date');
-
-  // Check for valid ISO format
-  if (!dateStr.match(isoRegex)) {
-    console.error('Invalid ISO date:', dateStr);
-    return;
-  }
-
-  // Parse date
-  let postDate = Date.parse(dateStr); 
-
-  // Handle invalid date 
-  if (isNaN(postDate)) {
-    console.error('Invalid date:', dateStr);
-    postDate = new Date(0); 
-  }
+  const postDate = new Date(dateStr);
 
   // Calculate and display difference
   const diffMs = Date.now() - postDate;
