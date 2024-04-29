9fc90dddf884bf422b748c6a3e72ca6296f071e1
Paul Berens <berens@gmail.com>
2024-04-29 13:50:38 -0700

    Create load-airtable-data.js

---
+++ assets/js/load-airtable-data.js
@@ -0,0 +1,11 @@
+document.getElementById('lookupForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const ID = document.getElementById('ID').value;
+
+  fetch(`https://nextjs-berenzino-h4zp0f8cd-pauls-projects-9cf86d6d.vercel.app/api/airtable-proxy.js?ID=${ID}`)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('result').innerHTML = JSON.stringify(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
