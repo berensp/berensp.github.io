4f668553610db8609f803c4b884e0645c03f6d6f
Paul Berens <berens@gmail.com>
2024-05-13 15:08:12 -0700

    Update load-airtable-data.js

--- assets/js/load-airtable-data.js
+++ assets/js/load-airtable-data.js
@@ -2,7 +2,7 @@ document.getElementById('lookupForm').addEventListener('submit', function(event)
   event.preventDefault();
   const ID = document.getElementById('ID').value;
 
-  fetch(`https://nextjs-berenzino-mcr8klf1b-pauls-projects-9cf86d6d.vercel.app/api/airtable-proxy.js?ID=${ID}`)
+  fetch(`https://nextjs-berenzino-baj7m2skx-pauls-projects-9cf86d6d.vercel.app/api/airtable-proxy.js?ID=${ID}`)
     .then(response => response.json())
     .then(data => {
       document.getElementById('result').innerHTML = JSON.stringify(data);
