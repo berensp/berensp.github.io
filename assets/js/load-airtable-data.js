74e1f6f1ea1e891ab750ee2234f9ed2d7b0a3083
Paul Berens <berens@gmail.com>
2024-05-24 21:33:28 -0700

    Update load-airtable-data.js

--- assets/js/load-airtable-data.js
+++ assets/js/load-airtable-data.js
@@ -2,7 +2,7 @@ document.getElementById('lookupForm').addEventListener('submit', function(event)
   event.preventDefault();
   const ID = document.getElementById('ID').value;
 
-  fetch(`https://supadupa-n62b1t4mo-pauls-projects-9cf86d6d.vercel.app/api/proxy.js?ID=${ID}`)
+  fetch(`https://supadupa-616uri0yp-pauls-projects-9cf86d6d.vercel.app/api/proxy.js?ID=${ID}`)
     .then(response => response.json())
     .then(data => {
       document.getElementById('result').innerHTML = JSON.stringify(data);
